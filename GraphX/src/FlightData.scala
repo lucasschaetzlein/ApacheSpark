6d942503ef1a12c0de8501b3b294f06460c7bb62
Lucas Schätzlein <lucasschatzlein@Lucass-MacBook-Air.local>
2017-05-07 20:45:08 +0200

    Added functionality to read csv file

--- GraphX/src/FlightData.scala
+++ GraphX/src/FlightData.scala
@@ -1,7 +1,15 @@
-
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.rdd.RDD
 
 object FlightData {
   def main(args: Array[String]) {
-    println("Hello World!");
+    val sparkConf = new SparkConf().setAppName("FlightData").setMaster("local[1]")
+    val sc = new SparkContext(sparkConf)
+    val spark = new SQLContext(sc)
+    
+    val df = spark.read.format("com.databricks.spark.csv").option("header", "true").load("../GraphX/ressource/2008.csv")
+    df.printSchema()
   }
 }
